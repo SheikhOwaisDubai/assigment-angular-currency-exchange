c0e0df3a8ce8c454b8924997c36481c8b3fd8b96
Muhammad Owais <owais.aimviz@gmail.com>
2023-05-08 01:56:28 +0400

    update currencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,13 +17,13 @@ export class AppComponent {
 
   changebase(a: string) {
     this.base = a;
-    console.log(this.base)
+  //  console.log(this.base)
   }
 
 
   tocountry(b: string) {
     this.cont2 = b;
-    console.log(this.cont2)
+  //  console.log(this.cont2)
   }
 
 
@@ -39,8 +39,63 @@ export class AppComponent {
     this.currency.GetCurrencyData(this.base).subscribe(data => {
 
       this.currjson = JSON.stringify(data);
-      console.log(this.currjson)
-      this.currjson
+      /* console.log(this.currjson)*/
+
+      this.currjson = JSON.parse(this.currjson);
+
+      //switch (this.cont2 == 'USD') {
+      //  this.retsult=this.currjson.rates.USD
+      //}
+    /*  alert(this.cont2)*/
+      //var asd=this.cont2;
+      //switch (this.cont2) {
+      //  case this.cont2:
+       
+      //    this.retsult = this.currjson.rates.
+      //  break;
+      
+      //default:
+      // code block
+    /*}*/
+      if (this.cont2 == 'INR') {
+        this.retsult = this.currjson.rates.INR
+      }
+
+      if (this.cont2 == 'USD') {
+        this.retsult = this.currjson.rates.USD
+      }
+
+      if (this.cont2 == 'PKR') {
+        this.retsult = this.currjson.rates.PKR
+      }
+
+      if (this.cont2 == 'AED') {
+        this.retsult = this.currjson.rates.AED
+      }
+
+
+      if (this.cont2 == 'EUR') {
+        this.retsult = this.currjson.rates.EUR
+      }
+
+
+      if (this.cont2 == 'GBP') {
+        this.retsult = this.currjson.rates.GBP
+      }
+
+
+      if (this.cont2 == 'AUD') {
+        this.retsult = this.currjson.rates.AUD
+      }
+
+      if (this.cont2 == 'SAR') {
+        this.retsult = this.currjson.rates.SAR
+      }
+
+      if (this.cont2 == 'KWD') {
+        this.retsult = this.currjson.rates.KWD
+      }
+
     });
 
   }
