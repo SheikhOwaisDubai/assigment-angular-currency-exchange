95bb95b4c784bf12a19292338c187a5356d434e6
Muhammad Owais <owais.aimviz@gmail.com>
2023-05-07 23:40:02 +0400

    latest updates currency api services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { CurrencyapiService } from './currencyapi.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'assigment-angular-currency-exchange';
+  currjson: any = [];
+
+  base = 'USD';
+  cont2 = 'USD';
+  retsult:string='1'
+
+
 }
---
+++ src/app/currencyapi.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CurrencyapiService {
+
+  constructor(private http: HttpClient) {
+
+  }
+  GetCurrencyData(countvar: string) {
+    let url = 'https://api.exchangerate.host/latest' + countvar;
+  }
+}
